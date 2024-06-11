e92392e18c8b514c0c4cd332146b057205038087
Justin Steffen <jcsteffen03@gmail.com>
2024-06-11 16:00:18 -0600

    2 test complete

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 /* Your Code Here */
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1] ,
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arr){
+    return arr.map(arr => {
+        return createEmployeeRecord(arr)
+    })
+}
+
+function createTimeInEvent(date_hour){
+
+    const [date, hour] = date_hour.split(" ")
+    return {
+        type: "TimeIn",
+        hour: hour,
+        date: date,
+    }
+    
+}
+//createTimeInEvent("2001-09-11 1200")
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
